be6a38acafc1bacbdefb51718eaf398c19c5e787
kagstaz <edwardkags@yahoo.co.uk>
2023-08-15 07:53:57 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower.c - Checks the lower case character
+ * @c: The character to be checked
+ * Return: 1 for lowercase character 0 for everything else
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
