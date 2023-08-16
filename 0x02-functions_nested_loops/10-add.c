6823b0fdaf0a3890d8d4a935543722e7da5d90ce
kagstaz <edwardkags@yahoo.co.uk>
2023-08-16 01:16:03 -0700

    integer sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @n1: first integer to be computed
+ * @n2: second integer to be computed
+ * Return: integer value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
