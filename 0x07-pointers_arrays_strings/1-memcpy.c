af86719e964127db53ca235a4a02391d775ae192
kagstaz <edwardkags@yahoo.co.uk>
2023-08-29 07:19:13 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _memcpy - function copies @n bytes from memory area @src
+* *_memcpy - function copies @n bytes from memory area @src
 * to memory area @dest
 *
 * @n: function copies
