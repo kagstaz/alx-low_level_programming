aa4cb9780ff54ade7e0ea93c40ddb7d87093ca8f
kagstaz <edwardkags@yahoo.co.uk>
2023-09-01 08:40:52 -0700

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the name of the program
+* @argc: number of arguements
+* @argv: array of arguements
+*
+* Return: Always 0 if successful
+*/
+int main(int, argc __attribute__ ((unused)), char *argv[])
+{
+		print("%s\n", *argv);
+
+		return (0);
+}
