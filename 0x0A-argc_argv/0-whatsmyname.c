97f344465663215d83941e20ae0fbef732b35e15
kagstaz <edwardkags@yahoo.co.uk>
2023-09-01 08:45:34 -0700

    print name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
 *
 * Return: Always 0 if successful
 */
-int main(int, argc __attribute__ ((unused)), char *argv[])
+int main(int, argc __attribute__((unused)), char *argv[])
 {
 		print("%s\n", *argv);
 
